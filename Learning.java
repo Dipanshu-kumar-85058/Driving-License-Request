41961341735f00c2816f045fc3b8f40bfb1f4efe
Dipanshu kumar <131379364+Dipanshu-kumar-85058@users.noreply.github.com>
2025-03-28 09:13:40 +0530

    Add files via upload

--- Learning.java
+++ Learning.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
+import java.util.Random;
+
 
 public class Learning {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to the Driving Licence Application");
 
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
@@ -19,8 +22,7 @@ public class Learning {
         if (age >= 18) {
             System.out.println("You are eligible for driving licence");
             limit = 1;
-        } 
-        
+        }
         else {
             System.out.println("You are NOT eligible for driving licence");
             limit = 0;
@@ -28,7 +30,7 @@ public class Learning {
 
         if (limit == 1) {
 
-        System.out.println("Enter other necessary details");
+        System.out.println("To apply for your Driving License,\nEnter other necessary details");
 
         System.out.print("Enter your phone number: ");
         long phoneNum1 = scanner.nextLong();
@@ -42,19 +44,24 @@ public class Learning {
 
             System.out.print("Enter your full Address: ");
             String address = scanner.nextLine();
+
+            // Reading /n error, adding scanner.nextLine()
             scanner.nextLine();
 
             System.out.print("Enter your vehicle Number/ID: ");
             String vehicleID = scanner.nextLine();
+
+            long referenceNum = random.nextInt(100000000, 1000000000);
+
+            System.out.println("Mr." + name + ", your request for a Driving License with with reference No. " + referenceNum + " has been submitted.");
+            System.out.println("You will be notified after the Completion of your Request");
         }
 
         }
         else{
             System.out.println("The age eligibility criteria is 18 or above");
         }
+        System.out.println("Thank you for using our service");
         scanner.close();
-
-        System.out.println("Mr." + name + ", your request for a Driving License with Necessary details has been submitted.");
-        System.out.println("You will be notified after the Completion of your Request");
     }
 }
